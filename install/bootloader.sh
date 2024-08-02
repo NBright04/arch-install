7e0402ff3a8e871a2b3e10c8f9dce9d812b412c1
NBright04 <71440500+NBright04@users.noreply.github.com>
2024-08-02 17:16:11 +0200

    deleted bootlader.sh added it into partition.sh

--- install/bootloader.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ROOTpart="$1"
-UUID=$(lsblk -dno UUID "/dev/$ROOTpart")
-
-if grep -q "^    CMDLINE=root=UUID=" ./limine.cfg; then
-    sudo sed -i "s/^    CMDLINE=root=UUID=[^ ]*/    CMDLINE=root=UUID=$UUID/" ./limine.cfg
-fi
--- install/partitions.sh
+++ install/partitions.sh
@@ -36,4 +36,8 @@ mount --mkdir "/dev/$HOMEpart" /mnt/home
 swapon "/dev/$SWAPpart"
 
 # bootloader preconfig
-./bootloader.sh "$ROOTpart"
+UUID=$(lsblk -dno UUID "/dev/$ROOTpart")
+
+if grep -q "^    CMDLINE=root=UUID=" ./limine.cfg; then
+    sudo sed -i "s/^    CMDLINE=root=UUID=[^ ]*/    CMDLINE=root=UUID=$UUID/" ./limine.cfg
+fi
